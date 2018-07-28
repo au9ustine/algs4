d8218cf1c79aeaf175073d9813db767d162ddacd
au9ustine <duke.augustine@gmail.com>
2018-07-28 21:57:37 +0800

    search :: BinarySearch :: fix test cases

--- src/main/java/com/github/au9ustine/algs4/search/BinarySearch.java
+++ src/main/java/com/github/au9ustine/algs4/search/BinarySearch.java
@@ -1,14 +1,22 @@
 package com.github.au9ustine.algs4.search;
 
+import java.util.Arrays;
+
 public class BinarySearch {
-    public static int search(Comparable[] a, Comparable key) {
+    public static int search(Comparable[] a, Comparable key, boolean sorted) {
+        assert sorted;
         int lo = 0, hi = a.length-1;
         while(lo <= hi) {
             int mid = (hi + lo) >>> 1;
-            if (key.compareTo(a[mid]) > 0) hi = mid - 1;
-            else if (key.compareTo(a[mid]) < 0) lo = mid + 1;
+            if (a[mid].compareTo(key) > 0) hi = mid - 1;
+            else if (a[mid].compareTo(key) < 0) lo = mid + 1;
             else return mid;
         }
         return -1;
+    }
+
+    public static int search(Comparable[] a, Comparable key) {
+        Arrays.sort(a);
+        return search(a, key, true);
     }
 }
--- src/main/test/com/github/au9ustine/algs4/search/BinarySearchTest.java
+++ src/main/test/com/github/au9ustine/algs4/search/BinarySearchTest.java
@@ -2,17 +2,43 @@ package com.github.au9ustine.algs4.search;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 
 public class BinarySearchTest {
 
     @Test
     public void search() {
-        String[] input = {"a", "b", "c", "d", "e"};
-        int actual = BinarySearch.search(input, "c");
-        int expected = 2;
+        String[] input = "abcdefghijklmnopqrstuvwxyz".split("");
+        int actual = BinarySearch.search(input, "w");
+        int expected = 22;
         assertEquals(expected, actual);
-        actual = BinarySearch.search(input, "f");
+        actual = BinarySearch.search(input, "0");
         assertEquals(-1, actual);
+    }
+
+
+    @Test
+    public void testExceptionalSearch() {
+        String[] input = "abcdefghijklmnopqrstuvwxyz".split("");
+        shuffle(input);
+        int actual = BinarySearch.search(input, "w");
+        int expected = 22;
+        assertEquals(expected, actual);
+    }
+
+    private static void shuffle(Comparable[] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            int r = i + (int)(Math.random() * (n-i));
+            swap(a, r, i);
+        }
+    }
+
+    private static void swap(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
     }
 }
