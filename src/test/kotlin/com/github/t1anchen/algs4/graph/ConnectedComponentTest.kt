94df4c89df05433e6ed9b698afbc7408cef64e16
au9ustine <duke.augustine@gmail.com>
2020-05-23 16:17:51 +0800

    add CC test

--- src/test/kotlin/com/github/t1anchen/algs4/graph/ConnectedComponentTest.kt
+++ src/test/kotlin/com/github/t1anchen/algs4/graph/ConnectedComponentTest.kt
@@ -2,6 +2,7 @@ package com.github.t1anchen.algs4.graph
 
 import com.github.t1anchen.algs4.utils.In
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ConnectedComponentTest {
 
@@ -10,6 +11,12 @@ class ConnectedComponentTest {
         val stream = In("/algs4-data/tinyG.txt")
         val g = Graph(stream)
         val cc = ConnectedComponent(g)
-        println(cc.toString())
+        val actual = cc.toString()
+        val expected = """3 components
+            |[0, 1, 2, 3, 4, 5, 6]
+            |[7, 8]
+            |[9, 10, 11, 12]""".trimMargin()
+        assertEquals(expected, actual)
+
     }
 }
